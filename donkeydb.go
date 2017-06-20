f94d34177ea03b03671f55721b215452b1d4d265
Gary Moore <gary.moore@gmail.com>
2017-06-20 16:28:44 -0400

    Index the keys

--- donkeydb.go
+++ donkeydb.go
@@ -7,6 +7,11 @@ import (
     "strings"
 )
 
+type Line struct {
+    key string
+    value string
+}
+
 func check(e error) {
     if e != nil {
         panic(e)
@@ -22,22 +27,29 @@ func insert(key string, value string) {
   f.Close()
 }
 
-func sselect(key string) string {
+func tokenizeLine(line string) Line {
+  tokens  := strings.Split(line, ",")
+  key     := tokens[0]  
+  val     := tokens[1]
+  return Line{key, val}
+}
+
+/*
+ * If our index is up to date, we should never need to rescan the file
+ */
+func fileScan(key string) string {
+  fmt.Printf("Scanning the whole file because this isn't in our index for some reason")
   var val string;
   f, err := os.OpenFile("./dat.donkey", os.O_RDONLY, 0644)
   check(err)
   scanner := bufio.NewScanner(f)
   for scanner.Scan() {
     line  := scanner.Text()
-    match := strings.HasPrefix(line, key)
+    tokenizedLine := tokenizeLine(line)
+    thisKey := tokenizedLine.key
 
-    if (match) {
-      tokens  := strings.Split(line, ",")
-      thisKey := tokens[0]
-
-      if (thisKey == key) {
-        val      = tokens[1]
-      }
+    if (thisKey == key) {
+      val      = tokenizedLine.value
     }
   }
   if err := scanner.Err(); err != nil {
@@ -48,8 +60,64 @@ func sselect(key string) string {
   return val
 }
 
+func filePosition(pos int) string {
+  fmt.Printf("Using index at line position %v\n", pos)
+  f, err := os.OpenFile("./dat.donkey", os.O_RDONLY, 0644)
+  check(err)
+  scanner := bufio.NewScanner(f)
+  x := 0
+  for scanner.Scan() {
+    line  := scanner.Text()
+    if (x == pos) {
+      return tokenizeLine(line).value
+    }
+    x++
+  }
+  if err := scanner.Err(); err != nil {
+    fmt.Fprintln(os.Stderr, "reading standard input:", err)
+  }
+
+  f.Close()
+  return "PANIC"
+}
+
+func sselect(key string, donkeyIndex map[string]int) string {
+
+  mapVal := donkeyIndex[key]
+
+  if (mapVal == 0) {
+    return fileScan(key)
+  } else {
+    return filePosition(mapVal)
+  }
+}
+
+func loadDonkeyIndex() map[string]int {
+  donkeyMap := make(map[string]int)
+
+  f, err := os.OpenFile("./dat.donkey", os.O_RDONLY, 0644)
+  check(err)
+  scanner := bufio.NewScanner(f)
+  x := 0
+  for scanner.Scan() {
+    line  := scanner.Text()
+    tokenizedLine := tokenizeLine(line)
+    donkeyMap[tokenizedLine.key] = x
+    x++
+  }
+  if err := scanner.Err(); err != nil {
+    fmt.Fprintln(os.Stderr, "reading standard input:", err)
+  }
+
+  f.Close()
+
+  return donkeyMap
+}
+
 func main() {
   numArgs := len(os.Args)
+
+  donkeyIndex := loadDonkeyIndex()
 
   if (numArgs < 3) {
     fmt.Println("Usage: donkeydb [command] [key] [value]")
@@ -63,7 +131,7 @@ func main() {
     value   := os.Args[3]
     insert(key, value)
   } else if (command == "select") {
-    val := sselect(key)
+    val := sselect(key, donkeyIndex)
     fmt.Println(val)
   } else {
     fmt.Println("I don't know how to " + command)
